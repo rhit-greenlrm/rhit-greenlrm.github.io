0d2de62a90ce066e6005c6f936b668a66f238a35
Rachel G <greenlrm@rose-hulman.edu>
2026-09-25 21:25:44 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 //inspiration and reference:  https://www.w3schools.com/howto/howto_css_flip_card.asp (this tutorial is for css, so i adapted it into js)
+// ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
 
 let photoCard = document.getElementById("laser");
 
 photoCard.addEventListener("click", function(){
-   if(photoCard.src == "laser.jpeg"){
+   if(photoCard.src.endsWith("laser.jpeg")){
        photoCard.src = "laser2.jpeg";
    } else {
      photoCard.src = "laser.jpeg";
