e2732a2fb368dc45917d692e60e1b363a22ed11b
Rachel G <greenlrm@rose-hulman.edu>
2026-09-25 21:11:38 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-//find useful interactive addition to use and cite source of IDEA (no code referncing)
+//inspiration and reference:  https://www.w3schools.com/howto/howto_css_flip_card.asp (this tutorial is for css, so i adapted it into js)
+
+let photoCard = document.getElementByID("laser");
+
+photoCard.addEventListener("click", function(){
+   if(photoCard.src = "laser.jpeg"){
+       photoCard.src = "laser2.jpeg";
+   } else {
+     photoCard.src = "laser.jpeg";
+   }
+});
+
